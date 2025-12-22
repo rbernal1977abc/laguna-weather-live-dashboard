3e97bb7a6662acd872a9d67df10635a80ec2952b
rpang888 <roland.pang888@gmail.com>
2025-12-22 19:50:53 +0800

    Update MapClient.js

--- MapClient.js
+++ MapClient.js
@@ -1,3 +1,24 @@
 import dynamic from 'next/dynamic';
-const MapInner = dynamic(() => import('./MapInner'), { ssr: false });
-export default function MapClient(props) { return <MapInner {...props} />; }
+
+// Load map only on client side (not during SSR)
+const MapInner = dynamic(() => import('./MapInner'), { 
+  ssr: false,
+  loading: () => (
+    <div style={{ 
+      height: '100%', 
+      display: 'flex', 
+      alignItems: 'center', 
+      justifyContent: 'center',
+      background: '#f0f0f0',
+      borderRadius: '8px'
+    }}>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ color: '#666' }}>Loading map...</div>
+      </div>
+    </div>
+  )
+});
+
+export default function MapClient(props) { 
+  return <MapInner {...props} />; 
+}
